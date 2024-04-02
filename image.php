78162354162fd942582329c85af789ee1f825cea
milenasavzyan <milena.savzyan21@gmail.com>
2024-04-02 11:53:54 +0400

    project 3

---
+++ image.php
@@ -0,0 +1,45 @@
+<?php
+
+if (isset($_POST['submit'])) {
+    if (isset($_FILES["image"]) && $_FILES["image"]["error"] == 0) {
+        $types = array("image/png", "image/jpg", "image/jpeg");
+        $file_type = $_FILES['image']['type'];
+
+        if (in_array($file_type, $allowed_types)) {
+            $target_dir = "image/";
+            $target_file = $target_dir . basename($_FILES["image"]["name"]);
+            $thumbnail_file = $target_dir . "thumbnail_" . basename($_FILES["image"]["name"]);
+
+            if (move_uploaded_file($_FILES["image"]["tmp_name"], $target_file)) {
+                list($width, $height) = getimagesize($target_file);
+                $new_width = 100;
+                $new_height = 100;
+                $thumb = imagecreatetruecolor($new_width, $new_height);
+
+                if ($file_type == "image/png")
+                    $source = imagecreatefrompng($target_file);
+                elseif ($file_type == "image/jpg")
+                    $source = imagecreatefromjpg($target_file);
+                elseif ($file_type == "image/jpeg")
+                    $source = imagecreatefromjpeg($target_file);
+
+                imagecopyresized($thumb, $source, 0, 0, 0, 0, $new_width, $new_height, $width, $height);
+
+                imagepng($thumb, $thumbnail_file);
+
+                echo "<h2>Original Image</h2>";
+                echo "<img src='$target_file' alt='Original Image'><br><br>";
+                echo "<h2>Thumbnail</h2>";
+                echo "<img src='$thumbnail_file' alt='Thumbnail'>";
+
+                imagedestroy($source);
+                imagedestroy($thumb);
+            } else {
+                echo "Error";
+            }
+        }
+    } else {
+        echo "Error";
+    }
+}
+
---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Image</title>
+</head>
+<body>
+<h2>Upload Image</h2>
+<form action="image.php" method="post" enctype="multipart/form-data">
+    <input type="file" name="image"><br>
+    <button type="submit" name="submit">Submit</button>
+</form>
+</body>
+</html>
+
+
